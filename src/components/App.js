3da5dd3ea083f1b99c5d48b0fdf120f56c2e00cf
mewarren <holla@mewarren.com>
2018-02-27 10:10:18 -0800

    config setup

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './app.css';
+
+export default class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {};
+
+  }
+  
+  render() {
+
+    return (
+     <h1>Hello React World!</h1>
+    );
+  }
+}
+
